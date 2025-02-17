f1759b4ab83d253ee898e2635e977500642d43fa
lukerocha <lucas.ar93@gmail.com>
2025-02-17 20:46:27 -0300

    Adds panels and jobs components

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,12 +1,12 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+<x-panel class="flex gap-x-6">
   <div>
     <x-employer-logo :width="100" />
   </div>
   
   <div class="flex-1 flex flex-col">
-    <a class="self-start text-sm text-gray-400">Laracasts</a>
+    <a class="self-start text-sm text-gray-400 ">Laracasts</a>
 
-    <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+    <h3 class=" group-hover:text-blue-800 font-bold text-xl mt-3 transition-colors duration-300">Video Producer</h3>
 
     <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
   </div>
@@ -16,4 +16,4 @@
       <x-tag>Tag</x-tag>
   </div>
 
-</div>
+</x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,10 +1,10 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<x-panel class="flex flex-col text-center">
   
   <div class="self-start text-sm">Laracasts</div>
 
-  <div class=" py-8 font-bold">
-      <h3>Video Producer</h3>
-      <p>Full Time - From $60,000</p>
+  <div class=" py-8">
+      <h3 class="font-bold group-hover:text-blue-800 text-xl transition-colors duration-300">Video Producer</h3>
+      <p class="text-sm mt-4">Full Time - From $60,000</p>
   </div>
 
   <div class="flex justify-between items-center mt-auto">
@@ -16,4 +16,4 @@
       </div>
       <x-employer-logo  :width="42" />
   </div>
-</div>
+</x-panel>
