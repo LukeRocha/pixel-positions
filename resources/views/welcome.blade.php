823dbb16a075987f56ae81f27439aecd5f961e63
lukerocha <lucas.ar93@gmail.com>
2025-02-17 22:37:21 -0300

    Finhishes main styles design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,14 @@
 <x-layout>
     <div class="space-y-10">
-        <section>
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Let's find a new Job for you!</h1>
+
+            <form action="">
+                <input type="text" placeholder="Web developer..." class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
+            </form>
+        </section>
+
+        <section class="pt-10">
             <x-section-heading >Featured Jobs</x-section-heading> 
 
             <div class="mt-6 grid lg:grid-cols-3 gap-8">
