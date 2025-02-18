0a892c74b4c073ff3ed011a061f5277fa7a43683
lukerocha <lucas.ar93@gmail.com>
2025-02-18 16:01:27 -0300

    adds unit test cases

--- tests/Pest.php
+++ tests/Pest.php
@@ -10,10 +10,11 @@
 | need to change it using the "pest()" function to bind a different classes or traits.
 |
 */
-
+//
 pest()->extend(Tests\TestCase::class)
  // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature');
+    ->use(\Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->in('Feature', 'Unit');
 
 /*
 |--------------------------------------------------------------------------
